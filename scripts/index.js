466e1ac74b4c72b9aa9393ffaa7ae483198d3822
Monti junior <montiabouou@gmail.com>
2024-04-04 13:15:57 -0800

    Library app ui partialy completed

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+const displayMessage = document.querySelector(".message");
+const formBtn = document.querySelector(".addForm");
+const submitBtn = document.querySelector("#submitBtn");
+const dialog = document.querySelector("dialog");
+const form = document.querySelector("form");
