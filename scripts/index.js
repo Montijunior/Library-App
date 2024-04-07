ce95d5d537f6402f7f7d28ce3e9061b0a7d541a2
Monti junior <montiabouou@gmail.com>
2024-04-07 13:22:30 -0800

    fix: can't figure out how to toggle the read status on the book cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,10 @@
-const displayMessage = document.querySelector(".message");
-const addNewBook = document.querySelector(".addForm");
-const modal = document.querySelector("dialog");
-const submitForm = document.querySelector("#submitBtn");
-const libraryContainer = document.querySelector(".cards-container");
+const addForm = document.querySelector(".addForm");
 const form = document.querySelector("form");
-const readBtn = document.getElementById("read");
+const submitBook = document.querySelector("#submitBtn");
+const modal = document.querySelector("#dialog");
+const libraryContainer = document.querySelector(".cards-container");
 
-// myLibrary array
+// Book library array
 const myLibrary = [];
 
 // Book constructor
@@ -16,60 +14,80 @@ function Book(title, author, pages){
   this.pages = pages;
 }
 
-function addToLibrary(){
+// add book to library
+function addBookToLibrary(){
   const title = document.getElementById("title");
   const author = document.getElementById("author");
   const pages = document.getElementById("pages");
 
-  // check if input is empty
-  if(title.value ==="" && author.value === "" & pages.value === ""){
+  // check if input.value is empty
+  if (title.value === "" && author.value === "" && pages.value === ""){
+    alert("Please fill out this form");
     return;
   } else {
+    // new Book instance
     const newBook = new Book(title.value, author.value, pages.value);
+
+    // push the newBook instance to myLibrary array
     myLibrary.push(newBook);
 
-    // update display
+    // render the books on the ui
     updateDisplay();
   }
 }
 
-// add to the ui 
+// render books ui
 function updateDisplay(){
-  //  loop through the myLibrary to find duplicate and insert to the ui
-  for (let i = 0; books = myLibrary, i <= myLibrary.length; i++){
-    libraryContainer.innerHTML += `
-    <div class="card" id=${i}>
+  // loop over the books in myLibrary to avoid duplicates
+  for (let i = 0; book = myLibrary, i <= myLibrary.length; i++){
+    // libraryContainer.innerHTML = "";
+
+    libraryContainer.innerHTML += 
+    `
+    <div class="card" id="${i}">
     <h3 class="title">${myLibrary[i].title}</h3>
     <p class="author">${myLibrary[i].author}</p>
     <p class="pages">${myLibrary[i].pages} Pages</p>
-    <button id="read" onclick="toggleRead(this)">Read</button>
-    <button class="deleteBtn" onclick="deleteBook(this)"><img src="icons/delete.svg" alt="delete svg">Delete</button>
-    </div>
+    <button id="read" onclick="toggleReadStatus(this)">Read</button>
+    <button class="deleteBtn" onclick="deleteBook(this)"><img src="icons/delete.svg" alt="delete svg icon">Delete</button>
+    </div> 
     `;
-    books.splice(myLibrary[i], 1);
+
+    // remove any duplicates
+    book.splice(myLibrary[i], 1);
   }
 }
 
-// delete books from the library 
+// toggleReadStatus(): can't figure out
+function toggleReadStatus(event){
+  if (event.target.innerText === "Read"){
+    event.target.innerText = "Not Read"
+  } else {
+    event.target.innerText = "Read"
+  }
+}
+
+// deleteBook function
 function deleteBook(button){
+  // find the book index
   const bookIndex = myLibrary.findIndex(
-    (book)=> book.id === button.parentElement.id
-  );
+    (book) => book.id === button.parentElement.id
+  )
+  
+  // remove from ui
   button.parentElement.remove();
   myLibrary.splice(bookIndex, 1);
 }
 
-
-
-
-addNewBook.addEventListener("click", ()=>{
+// open modal
+addForm.addEventListener("click", ()=>{
   form.reset();
   modal.showModal();
 });
 
-submitForm.addEventListener("click", (event)=>{
-  addToLibrary();
+// submit modal and form
+submitBook.addEventListener("click", (event)=>{
+  addBookToLibrary();
   event.preventDefault();
-  modal.close();
+  modal.close()
 });
-
